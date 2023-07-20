02c966d01d3ba0b862ddf3b082f9a3e139374c12
Alexandre Miranda Rezende Rocha <alexandremrr2014@gmail.com>
2023-07-19 21:58:49 -0300

    FEAT: Metodo de listagem de usuarios, template de database e readme

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -2,7 +2,38 @@
     class UserController extends BaseController{
         
         public function listAction(){
-            
+            $requestMethod = $_SERVER["REQUEST_METHOD"];
+            $stringParamsArray = $this->geStringParams();
+
+            if(strtoupper($requestMethod)  == 'GET'){
+                try {
+                    $userModel = new UserModel();
+                    $intLimit = 10;
+
+                    if(isset($stringParamsArray['limit']) && $stringParamsArray['limit']){
+                        $intLimit = $stringParamsArray['limit'];
+                    }
+
+                    $userArray = $userModel->getUsers($intLimit);
+                    $responseData = json_encode($usersArray);
+                } catch (Throwable $th) {
+                    throw $th->getMessage();
+                    return;
+                }
+            }else{
+                $errorDescription = 'Metodo não suportado';
+                $errorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+                $this->sendOutput(
+                    json_encode(array('error' => $errorDescription)),
+                    array('Content-Type: application/json', $errorHeader)
+                );
+                return;
+            }
+
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
         }
     }
 ?>
---
+++ README.md
@@ -0,0 +1,12 @@
+# API PHP PURO
+
+Criado uma pequena API de usuarios em PHP sem utilização de nenhum framework, projeto feito para fins de estudo e pratica
+
+## Aprendizados
+
+- PHP
+
+## Rodando localmente
+
+- Copiar e colar esse projeto na pasta htdocs do seu servidor Apache
+- E iniciar o seu servidor
